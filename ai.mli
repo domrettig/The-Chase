17ab3be3ac5f4f98610b437c111bdb1cb30b79ec
Mike <mhw66@cornell.edu>
2015-11-12 17:15:19 -0500

    mli for ai and convert

--- ai.mli
+++ ai.mli
@@ -1,1 +1,8 @@
-MIKE
+type probability
+
+(** Taked a difficulty as input (int 1, int 2,int 3 respectively)
+returns the range of probability of making a correct guess*)
+val difficulty : int -> probability
+
+(* This determines the correctness of he AI's response *)
+val is_correct : probability -> Reader.question -> bool
--- convert.mli
+++ convert.mli
@@ -1,1 +1,8 @@
-MIKE
+open Yojson.Basic.util
+
+(* Takes in the players question, and answer as well as the
+difficulty of said question and returns *)
+val parse_input : string -> string -> int -> Yojson.Basic.json
+
+(* Appends the new question to the set of rivia questions *)
+val write_to_file : Yojson.Basic.json -> unit
