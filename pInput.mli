f2f54ee43219b6acae4bb609f6ae24d5b0d11a22
Joe Mileo <joemileo@gmail.com>
2015-12-01 20:59:16 -0500

    updated mli for pInput

--- pInput.mli
+++ pInput.mli
@@ -16,7 +16,8 @@ val strip : string -> string
 val one_word_ans : Reader.question -> string -> bool
 
 (* Breaks down correct answer into single words and ensures that each word is
- * in the player's answer. Will call one_word_ans *)
+ * in the player's answer.
+ * i.e., the correct answer is a subset of the player's answer  *)
 val long_ans : Reader.question -> string -> bool
 
 (* Given a player's response and a question, determines if they were correct. 
